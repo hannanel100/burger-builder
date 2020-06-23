b59e3a7872d00349f8a284f002881a6119045a03
hannanel gershinsky <hannanel100@gmail.com>
2020-06-23 22:51:06 +0300

    finished 328

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,43 +1,51 @@
-import axios from 'axios'
+import axios from "axios";
 
-import * as actionTypes from './actionTypes';
+import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START
-    }
-}
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
 
 export const authSuccess = (authData) => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
-        authData: authData
-    }
-}
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData,
+  };
+};
 
 export const authFail = (error) => {
-    return {
-        type: actionTypes.AUTH_FAIL,
-        error: error
-    }
-}
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
+};
 
-export const auth = (email, password) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[AIzaSyCj6d9PEjNypBQXXCuP5r2hNy2Nw-3DWP4]', authData)
-        .then(response => {
-            console.log(response)
-            dispatch(authSuccess(response.data))
-        })
-        .catch(err=> {
-            console.log(err)
-            dispatch(authFail(err))
-        })
-}
-}
+export const auth = (email, password, isSignup) => {
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    console.log(authData);
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCj6d9PEjNypBQXXCuP5r2hNy2Nw-3DWP4";
+    if (!isSignup) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCj6d9PEjNypBQXXCuP5r2hNy2Nw-3DWP4";
+    }
+    axios
+      .post(url, authData)
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
+  };
+};
